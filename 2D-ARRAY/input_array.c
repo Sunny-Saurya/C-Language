926c9ee53a2a6b50dec5f35389e98b64c4c0fbe1
Sunny | Engineering Student | Computer Science Enthusiast <137380955+Sunny-Saurya@users.noreply.github.com>
2024-04-06 22:04:44 +0530

    Add files via upload

---
+++ 2D-ARRAY/input_array.c
@@ -0,0 +1,55 @@
+// // #include<stdio.h>
+// // int main()
+// // {
+// //     int arr[3][3];
+// //     for(int i = 0; i<3; i++)
+// //     {
+// //         for(int j = 0; j<3; j++)
+// //         {
+// //             scanf("%d",&arr[i][j]);
+            
+// //         }
+// //     }
+// //     for(int i = 0; i<3; i++)
+// //     {
+// //         for(int j = 0; j<3; j++)
+// //         {
+// //             printf("%d ",arr[i][j]);
+            
+// //         }
+// //         printf("\n");
+// //     }
+// // }
+
+// // TAKING THE NUMEBR OF ROWS AND COLUMSN FORM THE USER.
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int r,c;
+//     printf("enter the number of rows and columns: ");
+//     scanf("%d%d",&r,&c);
+
+//     int arr[r][c];
+
+//     for(int i = 0; i<r; i++)
+//     {
+//         for(int j = 0; j<c; j++)
+//         {
+//             scanf("%d",&arr[i][j]);
+            
+//         }
+//     }
+//     for(int i = 0; i<r; i++)
+//     {
+//         for(int j = 0; j<c; j++)
+//         {
+//             printf("%d ",arr[i][j]);
+            
+//         }
+//         printf("\n");
+//     }
+// }
+
+
