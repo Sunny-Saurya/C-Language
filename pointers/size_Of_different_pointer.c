0ee10ad28d453e9ed7b69b9908602718ae35be1a
Sunny | Engineering Student | Computer Science Enthusiast <137380955+Sunny-Saurya@users.noreply.github.com>
2024-04-23 22:25:17 +0530

    Add files via upload

---
+++ pointers/size_Of_different_pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int *ptr1;
+    char *ptr2;
+    float *ptr3;
+    double *ptr4;
+    long *ptr5;
+
+    printf("The size of int pointer is %d\n",sizeof(ptr1));
+    printf("The size of char pointer is %d\n",sizeof(ptr2));
+    printf("The size of float pointer is %d\n",sizeof(ptr3));
+    printf("The size of double pointer is %d\n",sizeof(ptr4));
+    printf("The size of long pointer is %d\n",sizeof(ptr5));
+
+}
