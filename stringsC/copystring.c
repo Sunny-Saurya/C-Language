211c3d83847691a4b434b14bf310453a32e63503
Sunny | Engineering Student | Computer Science Enthusiast <137380955+Sunny-Saurya@users.noreply.github.com>
2024-05-02 01:06:43 +0530

    Add files via upload

---
+++ stringsC/copystring.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char s1[] = "Physics Wallah";
+    // char *s2 = s1;
+    // s1[0] = 'M';
+    // printf("%s",s2); // s2 is a shallow copy
+
+    char s2[] = "physics wallah";
+}
